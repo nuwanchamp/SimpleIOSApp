590056484f87a4bfe66ca773b19bee7a81b1ae1b
nuwanchamp <nuwanchamp02@gmail.com>
2022-11-07 20:59:54 -0800

    Initial Commit

---
+++ MyIOS/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  MyIOS
+//
+//  Created by user229897 on 11/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let textLabel:UILabel = {
+        let label:UILabel = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Hello World"
+        return  label
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        textLabel.textColor = .blue
+        view.addSubview(textLabel)
+        textLabel.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        textLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        textLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    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
+}
+
